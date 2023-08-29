1a8a029aeeb51d96bd6837ba62eecfec954bbb20
Jason Moss <jmoss35@ut.utm.edu>
2023-08-29 12:05:39 -0500

    Created sum program.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,25 @@
 
 using namespace std;
 
+int sum(int num);
+
 int main() {
-  cout << "Hello World!";
-  
+  int num = 0;
+  cout << "Hello World!" << endl;
+  cout << "Enter a number: ";
+  cin >> num;
+  cout << endl;
+  cout << sum(num);
   return 0;
+}
+int sum(int num){
+  int add = 1;
+  int second = add + 1;
+  int sum = 0;
+  for(int i = 0; i < num; i++){
+    sum = sum + add + second;
+    add++;
+  }
+  
+  return sum;
 }
